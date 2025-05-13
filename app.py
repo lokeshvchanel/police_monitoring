ae203c3883eadf12d0973f0fd53108b2acf349e9
lokeshvchanel <lokeshchinnadurai@vchanel.com>
2025-05-13 12:13:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,6 +16,5 @@ def index():
     return render_template('index.html', data=data)
 
 if __name__ == '__main__':
-    app.run(debug=True)
-
-    app.run(debug=True, host='0.0.0.0', port=10000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
